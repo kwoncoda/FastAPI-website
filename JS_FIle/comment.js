4ce12b70525d09e38a437009df0ff531f7ce9100
Coda <onemore4111@gmail.com>
2024-08-14 00:04:23 +0900

    middle save

---
+++ JS_FIle/comment.js
@@ -0,0 +1,27 @@
+
+document.write('<script src="./JS_File/cookie.js"></script>');
+document.write('<script src="./JS_File/setting.js"></script>');
+const commentBtn = document.getElementById("submit-comment");
+
+
+// 제출 클릭시
+commentBtn.addEventListener("click",function(){
+
+    check_token();
+
+    let ACCESS_TOKEN = localStorage.getItem('access_token');
+    let VALUE = document.getElementById("comment-text").value;
+    
+    fetch(API_URL + "/commitupload",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({
+            text: VALUE,
+            access_token: ACCESS_TOKEN,
+        })
+    })
+    .then()
+})
